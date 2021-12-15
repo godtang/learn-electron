85a552c672b0ecb8f4a379c6c656bbb454cb3abb
tangmengjin <tangmengjin@laiye.com>
2021-12-15 20:16:44 +0800

    增加对文件的监控

--- renderer.js
+++ renderer.js
@@ -34,31 +34,76 @@ function loadFile(fileName) {
         .lines
         .forEach(
             function (line) {
-                let temp = JSON.parse(line.toString());
-                var tr = document.createElement('div');
-                var td1 = document.createElement('span');
-                var td2 = document.createElement('span');
-                tr.className = "tr";
-                td1.innerText = temp["timestamp"];
-                td1.className = "logTime";
-                var msg = temp["message"];
-                td2.innerText = msg;
-                if (typeof msg === "string") {
-                    try {
-                        msg = JSON.parse(msg);
-                        td2.innerText = JSON.stringify(msg, null, '\t');
-                    } catch (error) {
-                    }
-                } else {
-                    td2.innerText = JSON.stringify(msg, null, '\t');
-                }
-
-                td2.className = "logMsg";
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                table.appendChild(tr);
+                insertLine(line);
             }
         );
+    watchFile(fileName);
+}
 
+async function watchFile(filename) {
+    console.log('watchFile');
+    fs.open(filename, 'r', function (error, fd) {
+        var buffer;
+        var remainder = null;
+        fs.watchFile(filename, {
+            persistent: true,
+            interval: 1000
+        }, function (curr, prev) {
+            console.log(curr);
+            if (curr.mtime > prev.mtime) {
+                if (curr.size - prev.size > 0) {
+                    // 显示最新添加的文件内容
+                    //文件内容有变化，那么通知相应的进程可以执行相关操作。例如读物文件写入数据库等
+                    buffer = new Buffer.alloc(curr.size - prev.size);
+                    fs.read(fd, buffer, 0, (curr.size - prev.size), prev.size, function (err, bytesRead, buffer) {
+                        generateTxt(buffer.toString())
+                    });
+                } else if (curr.size - prev.size < 0) {
+                    // 文件删除了部分数据，需要重新加载
+                } else {
+                    //没有变化
+                }
+            } else {
+                console.log('文件读取错误');
+            }
+        });
+
+    });
+}
+
+function generateTxt(str) { // 处理新增内容的地方
+    var temp = str.split('\r\n');
+    var table = document.querySelector("body > div");
+    for (var s in temp) {
+        insertLine(temp[s]);
+    }
+}
+
+function insertLine(text) {
+    if ('' === text) return;
+    var table = document.querySelector("body > div");
+    let temp = JSON.parse(text.toString());
+    var tr = document.createElement('div');
+    var td1 = document.createElement('span');
+    var td2 = document.createElement('span');
+    tr.className = "tr";
+    td1.innerText = temp["timestamp"];
+    td1.className = "logTime";
+    var msg = temp["message"];
+    td2.innerText = msg;
+    if (typeof msg === "string") {
+        try {
+            msg = JSON.parse(msg);
+            td2.innerText = JSON.stringify(msg, null, '\t');
+        } catch (error) {
+        }
+    } else {
+        td2.innerText = JSON.stringify(msg, null, '\t');
+    }
+
+    td2.className = "logMsg";
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr);
 }
 
